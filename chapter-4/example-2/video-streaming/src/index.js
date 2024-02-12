873e20ba45a94924af6bdc90f0957b6d37b4a52c
jenakxm <jenakxm@gmail.com>
2024-02-12 23:39:48 +0900

    chapter-4 in progress

---
+++ chapter-4/example-2/video-streaming/src/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const AWS = require("aws-sdk");
+const http = require("http");
+
+const app = express();
+
+if (!process.env.PORT) {
+    throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
+}
+
+if (!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY) {
+    throw new Error("Please specify AWS access key and secret key in environment variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY.");
+}
+
+const PORT = process.env.PORT;
+const VIDEO_STORAGE_BUCKET = "microsvc-video-streaming"; // Replace with your S3 bucket name
+
+console.log(`Forwarding video requests to AWS S3 bucket: ${VIDEO_STORAGE_BUCKET}.`);
+
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
+app.get("/video", (req, res) => {
+    const videoPath = "SampleVideo_1280x720_1mb.mp4"; // Video path is hard-coded for the moment.
+
+    const params = {
+        Bucket: VIDEO_STORAGE_BUCKET,
+        Key: videoPath
+    };
+
+    const forwardRequest = s3.getObject(params).createReadStream();
+    forwardRequest.pipe(res);
+});
+
+app.listen(PORT, () => {
+    console.log(`Microservice online`);
+});
