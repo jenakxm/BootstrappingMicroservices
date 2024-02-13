92c58b6c5f60b4b51b9427545440b97ff5088c92
EC2 Default User <ec2-user@ip-172-31-4-10.ap-northeast-2.compute.internal>
2024-02-13 10:18:29 +0000

    chapter-4 aws conversion

--- chapter-4/example-2/video-streaming/src/index.js
+++ chapter-4/example-2/video-streaming/src/index.js
@@ -4,12 +4,13 @@ const http = require("http");
 
 const app = express();
 
+
 if (!process.env.PORT) {
     throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
 }
 
 if (!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY) {
-    throw new Error("Please specify AWS access key and secret key in environment variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY.");
+    throw new Error("[Streaming] Please specify AWS access key and secret key in environment variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY.");
 }
 
 const PORT = process.env.PORT;
@@ -17,10 +18,12 @@ const VIDEO_STORAGE_BUCKET = "microsvc-video-streaming"; // Replace with your S3
 
 console.log(`Forwarding video requests to AWS S3 bucket: ${VIDEO_STORAGE_BUCKET}.`);
 
-const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-});
+function createS3Client() {
+    return new AWS.S3({
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    });
+}
 
 app.get("/video", (req, res) => {
     const videoPath = "SampleVideo_1280x720_1mb.mp4"; // Video path is hard-coded for the moment.
@@ -29,6 +32,8 @@ app.get("/video", (req, res) => {
         Bucket: VIDEO_STORAGE_BUCKET,
         Key: videoPath
     };
+    
+    const s3 = createS3Client();
 
     const forwardRequest = s3.getObject(params).createReadStream();
     forwardRequest.pipe(res);
